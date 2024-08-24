47b3d07bb9e00a259b334f72bd3c41a39031083c
Julian22222 <julik.golovenj@gmail.com>
2024-08-24 20:46:22 +0100

    F-end

--- electronics/src/App.js
+++ electronics/src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import logo from "./IMG/phone-laptop-logo.jpg";
+import ItemForm from "./components/ItemForm";
 
 function App() {
+  const [allItems, setAllItems] = useState([
+    {
+      Id: "1",
+      Image: "./IMG/phone-laptop-logo.jpg",
+      Name: "OnePlus",
+      Type: "Phone",
+      Description:
+        "Smart phone RAM 16Gb Storage 1TB, camera 32MP, Colour: Black",
+      Price: 300,
+    },
+    {
+      Id: "2",
+      Image: "./IMG/phone-laptop-logo.jpg",
+      Name: "Huawei",
+      Type: "Phone",
+      Description:
+        "Smart phone RAM 16Gb Storage 1TB, camera 48MP, Colour: Green",
+      Price: 500,
+    },
+    {
+      Id: "3",
+      Image: "/IMG/phone-laptop-logo.jpg",
+      Name: "Xiaomi",
+      Type: "Phone",
+      Description:
+        "Smart phone RAM 16Gb Storage 1TB, camera 48MP, Colour: White",
+      Price: 400,
+    },
+    {
+      Id: "4",
+      Image: "~/IMG/phone-laptop-logo.jpg",
+      Name: "HP",
+      Type: "Laptop",
+      Description: "Laptop RAM 16Gb Storage 1TB, camera 16MP, Colour: Black",
+      Price: 700,
+    },
+    {
+      Id: "5",
+      Image: "./IMG/phone-laptop-logo.jpg",
+      Name: "Acer",
+      Type: "Laptop",
+      Description: "Laptop RAM 16Gb Storage 1TB, camera 32MP, Colour: Black",
+      Price: 900,
+    },
+  ]);
+
+  const [showForm, setShowForm] = useState("false");
+
+  const OpenForm = () => {
+    setShowForm(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {showForm ? <ItemForm setShowForm={setShowForm} /> : null}
+      <div className={showForm ? "BlurContent" : ""}>
+        <h2 class="Header">
+          Electrical goods, computing, phones and accessories in one place
+        </h2>
+        <div className="AddItemBtn-Container">
+          <button className="addItemBtn" onClick={OpenForm}>
+            Add Item
+          </button>
+        </div>
+        <table>
+          <tr>
+            <th className="ImageTable" width="200">
+              Image
+            </th>
+            <th className="NameTable">Name</th>
+            <th className="TypeTable">Type</th>
+            <th className="DescriptionTable">Description</th>
+            <th className="PriceTable">Price</th>
+          </tr>
+          {allItems?.map((el) => {
+            return (
+              <tr>
+                <td className="Imagetable">
+                  <img src={logo} alt="logo" width="200" />
+                </td>
+                <td className="NameTable">{el.Name}</td>
+                <td className="TypeTable">{el.Type}</td>
+                <td className="DescriptionTable">{el.Description}</td>
+                <td className="PriceTable">£ {el.Price}</td>
+                <div className="ActionBtn">
+                  <button>Edit</button>
+                  <button>Delete</button>
+                </div>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
     </div>
   );
 }
---
+++ electronics/src/components/ItemForm.jsx
@@ -0,0 +1,59 @@
+const ItemForm = (props) => {
+  const { setShowForm } = props;
+
+  const HandleCancel = () => {
+    setShowForm(false);
+  };
+
+  return (
+    <div className="FormContainer">
+      <label>Image</label>
+      <input
+        type="file"
+        name="image"
+        id="image"
+        className="Image"
+        required="required"
+      ></input>
+
+      <label>Name</label>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        placeholder="Insert name of the item"
+        className="Name"
+        required="required"
+      ></input>
+
+      <label for="type">Type</label>
+      <select name="type" id="type">
+        <option value=""></option>
+        <option value="phone">Phone</option>
+        <option value="laptop">Laptop</option>
+        <option value="tablet">Tablet</option>
+        <option value="watch">Smart Watch</option>
+        <option value="camera">Action Camera</option>
+        <option value="speaker">Wireless Bluetooth Speaker</option>
+      </select>
+
+      <label>Description</label>
+      <textarea
+        type="text"
+        name="description"
+        id="description"
+        className="Description"
+        placeholder="Put your description here"
+        required="required"
+      />
+
+      <button>Submit</button>
+      <button onClick={HandleCancel}>Cancel</button>
+
+      {/* <label htmlFor="Price"></label>
+      <input></input> */}
+    </div>
+  );
+};
+
+export default ItemForm;
